eb1a113f6bee32e56dc00c3849ba262fee530e2a
25011908vardhan <106120007@nitt.edu>
2021-06-14 19:02:15 +0530

    Modified Task1_A

--- Task1/Task1_A.c
+++ Task1/Task1_A.c
@@ -2,6 +2,27 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+int check(char a[],char b[],int n)
+{
+    int num1=0,num2=0,k,i;
+    for(i=n-1;i>=0;i--)
+    {
+        if(a[i]=='0')
+            k=0;
+        else
+            k=1;
+        num1+=pow(2,n-1-i)*k;
+         if(b[i]=='0')
+            k=0;
+        else
+            k=1;
+        num2+=pow(2,n-1-i)*k;
+    }
+    if(num2-num1==2)
+        return 1;
+    else
+        return 0;
+}
 
 int main() {
 
@@ -35,7 +56,10 @@ int main() {
             break;
         }
     }
+    if(check(prev,next,l))
     printf("%s %s",prev,next);
-    
+    else
+        printf("-1");
     return 0;
 }
+
