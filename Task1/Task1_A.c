c1a79283b27a90580fda895a026a16b1092587d0
25011908vardhan <106120007@nitt.edu>
2021-06-14 20:16:52 +0530

    Fixed one final bug

--- Task1/Task1_A.c
+++ Task1/Task1_A.c
@@ -2,36 +2,20 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-int check(char a[],char b[],int n)
-{
-    int num1=0,num2=0,k,i;
-    for(i=n-1;i>=0;i--)
-    {
-        if(a[i]=='0')
-            k=0;
-        else
-            k=1;
-        num1+=pow(2,n-1-i)*k;
-         if(b[i]=='0')
-            k=0;
-        else
-            k=1;
-        num2+=pow(2,n-1-i)*k;
-    }
-    if(num2-num1==2)
-        return 1;
-    else
-        return 0;
-}
-
 int main() {
 
    int l;
     scanf("%d",&l);
     char s[l+1],prev[l+1],next[l+1];
     scanf("%s",s);
+     if(l==1 && s[0]=='0')
+    {
+        printf("-1");
+        return 0;
+    }
     strcpy(prev,s);
     strcpy(next,s);
+   
     for(int i=l-1;i>=0;i--)
     {
         if(s[i]=='0')
@@ -56,7 +40,7 @@ int main() {
             break;
         }
     }
-    if(check(prev,next,l))
+    if(prev[0]!='0'&&next[0]!='0')
     printf("%s %s",prev,next);
     else
         printf("-1");
